0422288a6c591de82673da6e38d5692fa3bd8d2e
白延照 <VM-WIN7>
2020-12-01 15:31:35 +0800

    3.11.0.0 调整并行LFS

--- VMS/VMS/Common/FileExportFilter.cs
+++ VMS/VMS/Common/FileExportFilter.cs
@@ -16,47 +16,48 @@ namespace VMS
 
 		protected override void Clean(string path, string root, Stream input, Stream output)
 		{
-			var process = ProcLFS("clean", path, root, input);
+			using var process = new Process();
+			StartLFS(process, "clean", path, root, input);
 			process.WaitForExit(); //wait for git-lfs to finish
 
-			// write git-lfs pointer for 'clean' to git or file data for 'smudge' to working copy
+			//write git-lfs pointer for 'clean' to git
 			process.StandardOutput.BaseStream.CopyTo(output);
 			process.StandardOutput.BaseStream.Flush();
 			process.StandardOutput.Close();
 			output.Flush();
 			output.Close();
-			process.Dispose();
 		}
 
 		protected override void Smudge(string path, string root, Stream input, Stream output)
 		{
-			var process = ProcLFS("smudge", path, root, input);
-			output.Close(); //No used, create a new Prarallel Stream
+			var buffer = new MemoryStream((int)input.Length);
+	
+			output.Close(); //No used, create a new Prarallel Stream Instead
+			input.CopyTo(buffer);
+			buffer.Position = 0;
 			View.ProgressWindow.CreatePrarallel(delegate
 			{
 				try
 				{
 					View.ProgressWindow.Update(path);
+					using var process = new Process();
+					StartLFS(process, "smudge", path, root, buffer);
+
 					using var stream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None);
-					process.StandardOutput.BaseStream.CopyTo(stream);
+					process.StandardOutput.BaseStream.CopyTo(stream); //write file data for 'smudge' to working copy
 					process.StandardOutput.BaseStream.Flush();
 					process.StandardOutput.Close();
+					process.WaitForExit();
 				}
 				catch(Exception e)
 				{
 					MessageBox.Show("LFS Smudge Error: " + e.Message);
 				}
-				finally
-				{
-					process.WaitForExit();
-					process.Dispose();
-				}
 			});
 		}
 
-		private static Process ProcLFS(string mode, string path, string root, Stream input)
+		private static void StartLFS(Process process, string mode, string path, string root, Stream input)
 		{
-			var process = new Process();
 			process.StartInfo.FileName = "git";
 			process.StartInfo.Arguments = string.Format("lfs {0} {1}", mode, path);
 			process.StartInfo.WorkingDirectory = root + "/.git/";
@@ -67,11 +68,10 @@ namespace VMS
 			process.StartInfo.UseShellExecute = false;
 			process.Start();
 
-			input.CopyTo(process.StandardInput.BaseStream); //Smudge: write git-lfs pointer to stdin; Clean: write file data to stdin
+			input.CopyTo(process.StandardInput.BaseStream); //write file data to stdin -or- write git-lfs pointer to stdin
 			input.Flush();
 			process.StandardInput.Flush();
 			process.StandardInput.Close();
-			return process;
 		}
 	}
 }
